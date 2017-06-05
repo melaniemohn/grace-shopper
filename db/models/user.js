09d84c6ff9d6841a003a9d878333585c1909a27a
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-05 17:53:34 -0400

    finishing user model, still need associations

--- db/models/user.js
+++ db/models/user.js
@@ -1,9 +1,8 @@
 'use strict'
 
 // bcrypt docs: https://www.npmjs.com/package/bcrypt
-// okay we're about to start making this model
 const bcrypt = require('bcryptjs')
-    , {STRING, VIRTUAL} = require('sequelize')
+    , {STRING, VIRTUAL, BOOLEAN} = require('sequelize')
 
 module.exports = db => db.define('users', {
   name: STRING,
@@ -17,7 +16,13 @@ module.exports = db => db.define('users', {
 
   // We support oauth, so users may or may not have passwords.
   password_digest: STRING, // This column stores the hashed password in the DB, via the beforeCreate/beforeUpdate hooks
-  password: VIRTUAL // Note that this is a virtual, and not actually stored in DB
+  password: VIRTUAL, // Note that this is a virtual, and not actually stored in DB
+  shipAddress: STRING,
+  paymentInfo: STRING,  // we might not use this?  think about stripe / paypal as alternative
+  isAdmin: {
+    type: BOOLEAN,
+    defaultValue: false
+  }
 }, {
   indexes: [{fields: ['email'], unique: true}],
   hooks: {
@@ -37,7 +42,7 @@ module.exports = db => db.define('users', {
 
 module.exports.associations = (User, {OAuth, Thing, Favorite}) => {
   User.hasOne(OAuth)
-  User.belongsToMany(Thing, {as: 'favorites', through: Favorite})
+  // User.belongsToMany(Thing, {as: 'favorites', through: Favorite})
 }
 
 function setEmailAndPassword(user) {
