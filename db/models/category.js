931d4bd0353d3f9269e799dbdbc9c3f2981b51fe
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-06 17:30:26 -0400

    fixed associations

--- db/models/category.js
+++ db/models/category.js
@@ -7,5 +7,5 @@ module.exports = db => db.define('categories', {
 })
 
 module.exports.associations = (Category, {Product}) => {
-  Category.hasMany(Product)
+  Category.belongsToMany(Product, {through: 'ProductCategory'})
 }
