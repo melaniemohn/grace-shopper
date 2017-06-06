80a774db8d3ceb29b30b6bba40afe96982926635
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-06 16:55:29 -0400

    oops

--- db/models/index.js
+++ db/models/index.js
@@ -15,7 +15,6 @@ const app = require('APP')
     , metaModels = {
       OAuth: require('./oauth'),
       User: require('./user'),
-      Thing: require('./thing'),
       Favorite: require('./favorite'),
       // ---------- Add new models here ----------
       Product: require('./product')
