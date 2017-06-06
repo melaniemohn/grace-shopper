71010b722176429f5d1e6f943de3d2757e797098
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-06 12:52:27 -0400

    product model #3

---
+++ db/models/product.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const {STRING, INTEGER, DECIMAL, TEXT, ARRAY} = require('sequelize')
+
+module.exports = db => db.define('products', {
+  name: STRING,
+  picture: STRING,
+  price: {
+    type: DECIMAL(10, 2),
+    allowNull: false,
+    defaultValue: 0.0
+  },
+  description: TEXT,
+  categories: ARRAY(INTEGER), // this is an array of category IDs
+  // rating: DECIMAL(10, 1)  // add this later.  and write an instance method for setting average??
+  reviews: ARRAY(INTEGER),  // this is an array of review IDs
+
+}, {
+  instanceMethods: {
+    // add something here later to get average of ratings AND to get reviews??
+  }
+})
+
+module.exports.associations = (Product, {Review, Category}) => {
+  Product.hasMany(Review)
+  Product.hasMany(Category)
+}
