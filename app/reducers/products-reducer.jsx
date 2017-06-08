cc8dc7cf8854d5fa6484ff3b76b9773c6e635876
Kaisie Li <kaisinli@Kaisins-MacBook-Pro.local>
2017-06-08 17:32:31 -0400

    products reducers/container/component created

---
+++ app/reducers/products-reducer.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const initialProductsState = {
+  list: [],
+  selected: {}
+};
+
+/* ------------------------ REDUCER ------------------------ */
+const reducer = (state = initialProductsState, action) => {
+  switch (action.type) {
+  case AUTHENTICATED:
+    return action.user
+  }
+  return state
+}
+
+/* ------------------------ ACTIONS ------------------------ */
+const GETPRODUCTS = 'GET_PRODUCTS'
+const GETONEPRODUCT = 'GET_ONE_PRODUCT'
+
+/* ------------------------ ACTION CREATORS ------------------------ */
+export const getProducts = products => ({
+  type: GETPRODUCTS, products
+})
+
+export const getOneProduct = oneProduct => ({
+  type: GETONEPRODUCT, product
+})
+
+/* ------------------------ DISPATCHERS ------------------------ */
+export const fetchProducts = () =>
+  dispatch =>
+    axios.get('/api/products')
+      .then(res => dispatch(getProducts(res.data)))
+      .catch(err => console.error('Fetching products unsuccessful', err))
+
+export const fetchOneProducts = (productId) =>
+  dispatch =>
+    axios.get(`/api/products/${productId}`)
+      .then(res => dispatch(getProducts(res.data)))
+      .catch(err => console.error('Fetching product unsuccessful', err))
