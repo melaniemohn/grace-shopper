d1894e6db136c4733b2bc91629ec41f22882c68e
jasmine.zangi <jasmine.zangi@gmail.com>
2017-06-08 18:33:27 -0400

    updated add product

---
+++ app/components/addProduct.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export const Addproduct = ({ categories, addProduct }) => (
+  <form onSubmit={evt => {
+    evt.preventDefault()
+    addProduct(evt.target.product_name.value, evt.target.product_image.value, evt.target.product_price.value, evt.target.product_description.value, evt.target.categoryId.value[0])
+  }}>
+    <label>Product Name</label>
+    <input type="text" name="product_name"> </input>
+    <label>Product Picture</label>
+    <input type="file" name="product_image"></input>
+    <label>Product Price</label>
+    <input type="number" name="product_price"> </input>
+    <label>Product Description</label>
+    <textarea name="product_description"></textarea>
+    <select name="category" id="categoryId">
+      {
+        categories && categories.map((category) => (
+          <option key={category.id}>{category.id} {category.name}</option>
+        ))
+      }
+    </select>
+  </form>
+)
+import {addProduct} from 'App/app/reducers/product'
+import {connect} from 'react-redux'
+
+export default connect(
+  ({ categories }) => ({ categories: categories.list }),
+  {addProduct})(Addproduct)
+
+export const addProductBack = (name, image, price, description, categoryId) => {
+  return(dispatch,getState) =>{
+
+    return axios.post('/api/products', {name: name, picture: image, price: price, description: description, category_id: categoryId})
+      .then(product => {
+
+        dispatch(addProductFront(product.data));
+        hashHistory.push(`students/${student.id}`);
+      }).catch(()=>{
+
+      });
+
+  }
+
+
+
+
+}
