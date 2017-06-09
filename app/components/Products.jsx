74203e2495bd3d313940a9b09a5c06f20287fda1
JHFellows <j.fellows88@gmail.com>
2017-06-09 12:36:18 -0400

    all products are rendering #70

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const Products = (props) => {
-  console.log("PROPS", props)
   const products = props.products
-  console.log("PRODUCTS PROPS", products)
   return (
     <div>
       <h3>Products</h3>
       <div className="list-group">
-        {products && products.map(product => {
-          <h4>{product.name}</h4>
-        })}
+        {
+          products && products.map(product => (
+            <div key={ product.id }>
+              <h4>{product.name}</h4>
+              <h6>{product.price}</h6>
+              <h8>Description: {product.description}</h8>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
