a71164127b01028f8dfe046a5043d18e327086d6
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-11 01:50:05 -0400

    added icon to navbar

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -5,8 +5,8 @@ const Products = (props) => {
   const products = props.products
   return (
     <div>
-      <h3>Products</h3>
-      <div className="container">
+     <div className="container">
+        <h3>Products</h3>
         {
           products && products.map(product => (
             <div className="col-xs-4" key={ product.id }>
