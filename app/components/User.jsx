a9cf631a1d4b3076b0482348b8743631c6e2284d
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-12 19:07:00 -0400

    converting updated_at to an actual date

--- app/components/User.jsx
+++ app/components/User.jsx
@@ -6,13 +6,24 @@ const User = (props) => {
   const user = props.selectedUser
   const orders = props.selectedUser.orders
 
+  // note: we don't want to display orders with status of cart
+  // so, filter order.status !== 'cart'
+
+  // also write a function to get order date??
+  // convert date from updated_at to basic string
+
+  const convertDate = (date) => {
+    const dateArr = date.slice(0, 10).split('-')
+    return dateArr[1] + '-' + dateArr[2] + '-' + dateArr[0]
+  }
+
   return (
     <div className="container">
       <h3>{user.name}</h3>
       <h3>{user.email}</h3>
       <h3>Shipping address:</h3>
       <h3>{user.shipAddress}</h3>
-      <h3>Orders</h3>
+      <h3>Order History</h3>
       <table className="table">
         <thead>
           <tr>
@@ -24,7 +35,7 @@ const User = (props) => {
           {
             orders && orders.map(order => (
               <tr key={order.id}>
-                <td>oops order date here</td>
+                <td>{convertDate(order.updated_at)}</td>
                 <td>
                   <Link to={`/orders/${order.id}`}>{order.id}</Link>
                 </td>
