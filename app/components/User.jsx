3489c490b2e729c0115f28c8f84e71dc5e2651c5
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-12 20:17:17 -0400

    delete superfluous OrderHistory component (it's in User component now)

--- app/components/User.jsx
+++ app/components/User.jsx
@@ -6,11 +6,7 @@ const User = (props) => {
   const user = props.selectedUser
   const orders = props.selectedUser.orders
 
-  // note: we don't want to display orders with status of cart
-  // so, filter order.status !== 'cart'
-
-  // also write a function to get order date??
-  // convert date from updated_at to basic string
+  // MPM note: we don't want to display orders with status of cart, so filter order.status !== 'cart'
 
   const convertDate = (date) => {
     const dateArr = date.slice(0, 10).split('-')
@@ -20,9 +16,9 @@ const User = (props) => {
   return (
     <div className="container">
       <h3>{user.name}</h3>
-      <h3>{user.email}</h3>
-      <h3>Shipping address:</h3>
-      <h3>{user.shipAddress}</h3>
+      <h4>{user.email}</h4>
+      <h4>Shipping address:</h4>
+      <h4>{user.shipAddress}</h4>
       <h3>Order History</h3>
       <table className="table">
         <thead>
