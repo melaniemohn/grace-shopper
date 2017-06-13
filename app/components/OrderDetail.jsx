1c10310dec7bf6fa00cce936bb8bcbb16c091542
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-12 20:11:52 -0400

    order detail page is working

--- app/components/OrderDetail.jsx
+++ app/components/OrderDetail.jsx
@@ -7,26 +7,49 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchSingleOrder } from '../reducers/orders-reducer'
 
 // ----- single-order component -----
 
 export const OrderDetail = (props) => {
-  const order = fetchSingleOrder()
+  const order = props.selectedOrder
+  const items = props.selectedOrder.orderItems
+  console.log('items???', items)
+  // we *have* products...
+  // instead of looping over our product list (don't even map products to props!!)...
+  // ...let's rewrite the api route to include product data the way we did with users
   return (
     <div className="container">
-      <h3>Details for Order #</h3>
+      <h3>Details for Order #{order.id}</h3>
+      <h4>Order Status: {order.status}</h4>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            items && items.map(item => (
+              <tr key={item.id}>
+                <td>{item.product.name}</td>
+                <td>{item.price}</td>
+                <td>{item.quantity}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
 
 // ----- single-order container -----
 const mapStateToProps = (state) => ({
-  // I think we'll also want something about the user on state
   selectedOrder: state.orders.selected,
-  products: state.products.list
 })
 
-const mapDispatchToProps = ({ fetchSingleOrder })
+const mapDispatchToProps = null
 
 export default connect(mapStateToProps, mapDispatchToProps)(OrderDetail)
